a060a1de1d759b2c65976c8d116621f8af40774d
superultratyler <91762213+superultratyler@users.noreply.github.com>
2024-04-15 12:34:28 -0700

    Art added to suspect selection and ending screen added

--- Scripts/Screens/EndingScreen.gd
+++ Scripts/Screens/EndingScreen.gd
@@ -1,10 +1,13 @@
 extends Screen
 class_name EndingScreen
 
+@export var ending_narration : AudioStreamMP3
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Main.instance.audio_stream.stream = ending_narration
+	Main.instance.audio_stream.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
